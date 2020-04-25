849aca46a8345d061182856ab19242fb43a4b663
John Q. Splittist <splittist@splittist.com>
2020-04-25 20:19:17 +0200

    More Ginjish.

--- ginjish.lisp
+++ ginjish.lisp
@@ -161,26 +161,23 @@
 	       (list :if-else (fourth (second c)) (first c) (eighth (second c)))
 	       (first c))))
 
-(defrule or-test (or (and or-test ws "or" ws and-test)
-		     and-test)
+(defrule or-test (or or-test-sub and-test))
+
+(defrule or-test-sub (and or-test ws "or" ws and-test)
   (:lambda (o)
-	   (if (and (consp o) (equal "or" (third o)))
-	       (list :or (first o) (fifth o))
-	       o)))
+    (list :or (first o) (fifth o))))
 
-(defrule and-test (or (and and-test ws "and" ws not-test)
-		      not-test)
+(defrule and-test (or and-test-sub not-test))
+
+(defrule and-test-sub (and and-test ws "and" ws not-test)
   (:lambda (a)
-	   (if (and (consp a) (equal "and" (third a)))
-	       (list :and (first a) (fifth a))
-	       a)))
+    (list :and (first a) (fifth a))))
 
-(defrule not-test (or (and "not" ws not-test)
-		      comparison)
+(defrule not-test (or not-test-sub comparison))
+
+(defrule not-test-sub (and "not" ws not-test)
   (:lambda (n)
-    (if (and (consp n) (equal "not" (first n)))
-	(list :not (third n))
-	n)))
+    (list :not (third n))))
 
 (defun chain-comparisons (c)
   `(:and
@@ -189,13 +186,14 @@
 	 collecting (list op left right))))
 
 (defrule comparison (and or-expr
-			 (* (and ws* comp-operator ws* or-expr))) ;; FIXME
+			 (* (and ws* comp-operator ws* or-expr)))
   (:lambda (c)
-	   (if (null (second (second c)))
-	       (first c)
-	       (chain-comparisons c))))
+    (case (length (second c))
+      (0 (first c))
+      (1 (list (second (first (second c))) (first c) (fourth (first (second c)))))
+      (t (chain-comparisons c)))))
 
-(defrule comp-operator (or ">=" "<=" "!=" "<" ">" "="
+(defrule comp-operator (or ">=" "<=" "!=" "<" ">" "=="
 			   (and "is" (? (and ws "not")))
 			   (and (? (and "not" ws)) "in"))
   (:lambda (c)
@@ -209,72 +207,202 @@
 		:in))
 	(alexandria:make-keyword c))))
 
-(defrule or-expr (or (and or-expr ws* "|" ws* xor-expr)
-		     xor-expr)
+(defrule or-expr (or or-expr-sub xor-expr))
+
+(defrule or-expr-sub (and or-expr ws* "|" ws* xor-expr)
   (:lambda (o)
-    (if (and (consp o) (null (second o)))
-	(list (alexandria:make-keyword (third o)) (first o) (fifth o))
-	o)))
+    (list :\| (first o) (fifth o))))
 
-(defrule xor-expr (or (and xor-expr ws* "^" ws* and-expr)
-		      and-expr)
+(defrule xor-expr (or xor-expr-sub and-expr))
+
+(defrule xor-expr-sub (and xor-expr ws* "^" ws* and-expr)
   (:lambda (x)
-    (if (and (consp x) (null (second x)))
-	(list (alexandria:make-keyword (third x)) (first x) (fifth x))
-	x)))
+    (list (alexandria:make-keyword (third x)) (first x) (fifth x))))
 
-(defrule and-expr (or (and and-expr ws* "&" ws* shift-expr)
-		      shift-expr)
+(defrule and-expr (or and-expr-sub shift-expr))
+
+(defrule and-expr-sub (and and-expr ws* "&" ws* shift-expr)
   (:lambda (a)
-    (if (and (consp a) (null (second a)))
-	(list (alexandria:make-keyword (third a)) (first a) (fifth a))
-	a)))
+    (list (alexandria:make-keyword (third a)) (first a) (fifth a))))
 
-(defrule shift-expr (or (and shift-expr ws* (or "<<" ">>") ws* a-expr)
-			a-expr)
+(defrule shift-expr (or shift-expr-sub a-expr))
+
+(defrule shift-expr-sub (and shift-expr ws* (or "<<" ">>") ws* a-expr)
   (:lambda (s)
-    (if (and (consp s) (null (second s)))
-	(list (alexandria:make-keyword (third s)) (first s) (fifth s))
-	s)))
+    (list (alexandria:make-keyword (third s)) (first s) (fifth s))))
 
-(defrule a-expr (or (and a-expr ws* "+" ws* m-expr)
-		    (and a-expr ws* "-" ws* m-expr)
-		    m-expr)
+(defrule a-expr (or a-expr-sub m-expr))
+
+(defrule a-expr-sub (or (and a-expr ws* "+" ws* m-expr)
+			(and a-expr ws* "-" ws* m-expr))
   (:lambda (a)
-    (if (and (consp a) (null (second a)))
-	(list (alexandria:make-keyword (third a)) (first a) (fifth a))
-	a)))
+    (list (alexandria:make-keyword (third a)) (first a) (fifth a))))
 
-(defrule m-expr (or (and m-expr ws* "*" ws* u-expr)
-		    (and m-expr ws* "@" ws* u-expr)
-		    (and m-expr ws* "//" ws* u-expr)
-		    (and m-expr ws* "/" ws* u-expr)
-		    (and m-expr ws* "%" ws* u-expr)
-		    u-expr)
+(defrule m-expr (or m-expr-sub u-expr))
+
+(defrule m-expr-sub (or (and m-expr ws* "*" ws* u-expr)
+			(and m-expr ws* "@" ws* u-expr)
+			(and m-expr ws* "//" ws* u-expr)
+			(and m-expr ws* "/" ws* u-expr)
+			(and m-expr ws* "%" ws* u-expr))
   (:lambda (m)
-    (if (and (consp m) (null (second m)))
-	(list (alexandria:make-keyword (third m)) (first m) (fifth m))
-	m)))
+    (list (alexandria:make-keyword (third m)) (first m) (fifth m))))
 
-(defrule u-expr (or (and "-" ws* u-expr)
-		    (and "+" ws* u-expr)
-		    (and "~" ws* u-expr)
-		    power)
+(defrule u-expr (or u-expr-sub power))
+
+(defrule u-expr-sub (or (and "-" ws* u-expr)
+			(and "+" ws* u-expr)
+			(and "~" ws* u-expr))
   (:lambda (u)
-    (if (and (consp u) (member (first u) '(#\- #\+ #\~) :test #'string=))
-	(list (alexandria:make-keyword (first u)) (third u))
-	u)))
+    (list (alexandria:make-keyword (first u)) (third u))))
 
 (defrule power (and primary (? (and ws* "**" ws* u-expr)))
   (:lambda (p)
-	   (if (null (second p))
-	       (first p)
-	       (list :** (first p) (fourth (second p))))))
-
-;;; primary
-
-(defrule primary atom) ; attributeref subscription slicing call
+    (if (null (second p))
+	(first p)
+	(list :** (first p) (fourth (second p))))))
 
 ;;; atom
 
-(defrule atom (or identifier literal)) ; enclosure
+(defrule atom (or identifier literal enclosure))
+
+(defrule enclosure (or parenth-form list-display set-display dict-display))
+
+(defrule parenth-form (and "("  ws* (? expression-list) ws* ")")
+  (:function third))
+
+(defrule expression-list (and expression (* (and ws* "," ws* expression)) (? (and ws* ",")))
+  (:lambda (e)
+    (case (length (second e))
+      (0 (if (third e)
+	     `(:tuple ,(first e))
+	     (first e)))
+      (t (let ((elements (mapcar #'fourth (second e))))
+	   `(:tuple ,(first e) ,@elements)))))) 
+
+(defrule expression-list* (and expression (* (and ws* "," ws* expression)) (? (and ws* ",")))
+  (:lambda (e)
+    `(,(first e) ,@(mapcar #'fourth (second e)))))
+
+(defrule list-display (and "[" ws* (? expression-list*) ws* "]")
+  (:lambda (l)
+    `(:list ,@(third l))))
+
+(defrule set-display (and "{" ws* expression-list* ws* "}")
+  (:lambda (s)
+    `(:set ,@(third s))))
+
+(defrule dict-display (and "{" ws* (? key-datum-list) ws* "}")
+  (:lambda (d)
+    `(:dict ,@(third d))))
+
+(defrule key-datum-list (and key-datum (* (and ws* "," ws* key-datum)) (? (and ws* ",")))
+  (:lambda (k)
+    `(,(first k) ,@(mapcar #'fourth (second k)))))
+
+(defrule key-datum (and expression ws* ":" ws* expression)
+  (:lambda (k)
+    (cons (first k) (fifth k))))
+
+
+;;; primary
+
+(defrule primary (or attributeref subscription slicing call atom))
+
+(defrule attributeref (and primary "." identifier)
+  (:lambda (a)
+    (list :get-attr (first a) (third a))))
+
+(defrule subscription (and primary "[" ws* expression-list* ws* "]")
+  (:lambda (s)
+    (list :get-item (first s) (fourth s))))
+
+(defrule call (and primary "(" ws* (? expression-list*) ws* ")") ; FIXME lispy lambda list
+  (:lambda (c)
+    (list :call (first c) (fourth c))))
+
+(defrule argument-list (or keywords-arguments ; FIXME this doesn't seem to work
+			   (and positional-arguments (? (and ws* "," keywords-arguments))))
+  )
+
+(defrule argument-list (and (? positional-arguments) (? (and ws* "," keywords-arguments))))
+
+(defrule positional-arguments (and positional-item (* (and ws* "," ws* positional-item)))
+  (:lambda (p)
+    `(,(first p) ,@(mapcar #'fourth (second p)))))
+
+(defrule positional-item expression)
+
+(defrule keywords-arguments (and keyword-item (* (and ws* "," ws* keyword-item)))
+  (:lambda (k)
+    `(,(first k) ,@(mapcar #'fourth (second k)))))
+
+(defrule keyword-item (and identifier ws* "=" ws* expression)
+  (:lambda (k)
+    (cons (first k) (fifth k))))
+
+(defrule slicing (and primary "[" ws* slice-list ws* "]")
+  (:lambda (s)
+    (list :slicing (first s) (fourth s))))
+
+(defrule slice-list (and slice-item (* (and ws* "," ws* slice-item)) (? (and ws* ",")))
+  (:lambda (e)
+    (case (length (second e))
+      (0 (if (third e)
+	     `(:tuple ,(first e))
+	     (first e)))
+      (t (let ((elements (mapcar #'fourth (second e))))
+	   `(:tuple ,(first e) ,@elements))))))
+
+(defrule slice-item (or proper-slice expression))
+
+(defrule proper-slice (and (? expression) ws* ":" (? (and ws* expression)) (? (and ws* ":" (? (and ws* expression)))))
+  (:lambda (p)
+    (list :proper-slice (first p) (second (fourth p)) (second (third (fifth p))))))
+
+;;; template
+
+(defrule t-statement-start "{%")
+
+(defrule t-statement-end "%}")
+
+(defrule t-expression-start "{{")
+
+(defrule t-expression-end "}}")
+
+(defrule t-comment-start "{#")
+
+(defrule t-comment-end "#}")
+
+(defrule matter (+ (not (or t-statement-start t-expression-start t-comment-start)))
+  (:lambda (m)
+    (list :matter (text m))))
+
+(defrule template (* (or t-statement #+(or)t-expression t-comment matter)))
+
+;;; comment
+
+(defrule t-comment (and t-comment-start comment-content t-comment-end)
+  (:constant nil))
+
+(defrule comment-content (* (not t-comment-end)))
+
+(defrule t-expression (and t-expression-start ws* expression ws* t-expression-end)
+  (:function third))
+
+;;; statement
+    
+(defrule t-statement t-raw)
+
+(defrule t-raw (and t-raw-start raw-content t-raw-end)
+  (:lambda (r)
+    (list :raw (second r))))
+
+(defrule raw-content (* (not t-raw-end))
+  (:text t))
+
+(defrule t-raw-start (and t-statement-start ws* "raw" ws* t-statement-end)
+  (:constant nil))
+
+(defrule t-raw-end (and t-statement-start ws* "endraw" ws* t-statement-end)
+  (:constant nil))
